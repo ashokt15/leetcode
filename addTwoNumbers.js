7bd0572cb998e015d38518ecddc43210b3da7ae5
ashok <urashok15@gmail.com>
2022-07-21 13:58:31 +0530

    refactor add two numbers

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -4,36 +4,17 @@ function ListNode(val, next) {
 }
 var addTwoNumbers = function (l1, l2) {
     let current1 = l1, current2 = l2, prev, head, carryForward = 0;
-    while (current1 != null || current2 != null) {
+    while (current1 != null || current2 != null || carryForward == 1) {
         let a = current1 ? current1.val : 0;
         let b = current2 ? current2.val : 0;
         let number = a + b + carryForward;
 
         if (number > 9) {
-            if ((current1 === null || current1.next === null) && (current2 === null || current2.next === null)) {
-                while (number != 0) {
-                    digit = number % 10;
-                    node = new ListNode(digit, undefined);
-                    if (prev === undefined) {
-                        prev = node;
-                        head = node;
-                    } else {
-                        prev.next = node;
-                        prev = node;
-                    }
-                    number = Math.trunc(number / 10);
-                }
-                return head;
-            }
-            else {
-                carryForward = 1;
-                number = number % 10;
-            }
-
+            carryForward = 1;
+            number = number % 10;
         } else {
             carryForward = 0;
         }
-
         node = new ListNode(number, undefined);
         if (prev === undefined) {
             prev = node;
