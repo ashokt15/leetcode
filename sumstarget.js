ad00493eba969ab4e099a1a50db7f5f13e6f88b4
ashok <urashok15@gmail.com>
2022-07-21 17:31:24 +0530

    sumstarget

--- sumstarget.js
+++ sumstarget.js
@@ -1,10 +1,12 @@
 var twoSum = function (nums, target) {
+    var hashTable = {};
     for (i = 0; i < nums.length; i++) {
-        for (j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                return [i, j];
-            }
-
+        hashTable[nums[i]] = i;
+    } console.log(hashTable);
+    for (i = 0; i < nums.length; i++) {
+        if (hashTable.hasOwnProperty(target - nums[i])) {
+            if (i != hashTable[target - nums[i]])
+                return [i, hashTable[target - nums[i]]]
         }
     }
 }
